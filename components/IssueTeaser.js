970bc170fb62fd6af5b9b578d29bc4ba3544f805
James Moon <jamesfmoon@gmail.com>
2020-07-13 15:32:07 -0400

    fixed what I broke

--- components/IssueTeaser.js
+++ components/IssueTeaser.js
@@ -1,6 +1,6 @@
 // author, title, status, createdAt, id, severity
 import moment from "moment";
-import { Pane, Checkbox, Card } from "evergreen-ui";
+import { Pane, Checkbox, Card, Heading, Text } from "evergreen-ui";
 import React, { useState } from "react";
 
 export function IssueTeaser({ id, author, title, createdAt }) {
@@ -13,61 +13,25 @@ export function IssueTeaser({ id, author, title, createdAt }) {
       borderColor="silver"
       borderWidth="1px"
       borderStyle="solid"
-      style={{ "&:nthChild(1)": { content: `""`, backgroundColor: "red" } }}
     >
-      <Pane style={styles.left}>
-        <input
+      <Pane>
+        <Checkbox
           type="checkbox"
-          style={styles.input}
           onChange={(e) => setIsSelected(!isSelected)}
           checked={isSelected}
-        ></input>
+          marginTop="5px"
+        ></Checkbox>
       </Pane>
-      <Pane style={styles.right}>
-        <h3 style={styles.title}>{title}</h3>
-        <span style={{ ...styles.info }}>#{id} &nbsp;</span>
-        <span style={{ ...styles.info, ...styles.author }}>
-          created by {author} &nbsp;
-        </span>
-        <span style={{ ...styles.info, ...styles.dateMakeUp }}>
-          {moment(createdAt).fromNow()}
-        </span>
+      <Pane marginLeft="10px">
+        <Heading margin="0" size={600}>
+          {title}
+        </Heading>
+        <Text size={300}>
+          <span>#{id} &nbsp;</span>
+          <span>created by {author} &nbsp;</span>
+          <span>{moment(createdAt).fromNow()}</span>
+        </Text>
       </Pane>
     </Pane>
   );
 }
-
-let styles = {
-  title: {
-    color: "#234361",
-    margin: "0px",
-    fontSize: "1.5em",
-  },
-
-  container: {
-    border: "1px solid silver",
-    padding: "10px",
-  },
-
-  dateMakeUp: {
-    color: "#999",
-    fontSize: ".8em",
-  },
-
-  author: {
-    fontSize: ".9em",
-  },
-
-  input: {
-    width: "15px",
-    height: "15px",
-  },
-
-  left: {
-    padding: "8px 0 0 10px",
-  },
-
-  right: {
-    padding: "0 0 0 10px",
-  },
-};
