952c167815475ad707e463b8046b56275275b25f
root <jamesfmoon@gmail.com>
2020-07-26 17:37:15 +0000

    fixed alignment

--- components/IssueBox.js
+++ components/IssueBox.js
@@ -3,13 +3,9 @@ import { Pane, Text, Select } from "evergreen-ui";
 export function IssueBox() {
   return (
     //   Container that hold all of the IssueTeaser Components
-    <Pane display="Flex" background="overlay">
-      <Pane background-image="none">
-        <Select
-          onChange={(event) => alert(event.target.value)}
-          background-image="none"
-          box-shadow="none"
-        >
+    <Pane display="flex" background="overlay" justifyContent="flex-end">
+      <Pane>
+        <Select onChange={(event) => alert(event.target.value)}>
           <option value="foo" defaultValue>
             Foo
           </option>
